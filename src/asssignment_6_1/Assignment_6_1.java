815120d776f62c2db97fe3f02190fdfb3fee20b3
00 <00@10290-PC>
2015-01-26 11:07:53 -0500

    Update 1/26

--- src/asssignment_6_1/Assignment_6_1.java
+++ src/asssignment_6_1/Assignment_6_1.java
@@ -5,9 +5,9 @@ public class Assignment_6_1 {
         Scanner input = new Scanner (System.in);
         System.out.println("What are the names of the passengers ?");
         
-        String[][] grid = new String [4] [9];
-        System.out.println(grid);
-                return grid;
+        String[][] flightManifest = new String [4] [9];
+        System.out.println(flightManifest);
+                return flightManifest;
     }
     
 }
