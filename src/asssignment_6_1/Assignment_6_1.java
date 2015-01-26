07c7581a6a399a57ca0a26a626aee87060961b27
00 <00@10290-PC>
2015-01-26 10:55:24 -0500

    new commit

---
+++ src/asssignment_6_1/Assignment_6_1.java
@@ -0,0 +1,13 @@
+package asssignment_6_1;
+import java.util.Scanner;
+public class Assignment_6_1 {
+    public static String[][] Names(){
+        Scanner input = new Scanner (System.in);
+        System.out.println("What are the names of the passengers ?");
+        
+        String[][] grid = new String [4] [9];
+        System.out.println(grid);
+                return grid;
+    }
+    
+}
