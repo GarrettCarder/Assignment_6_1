11badea7cb9aa687f638c0c7f6bc6402c500598e
GarrettCarder <GarrettCarder@users.noreply.github.com>
2015-01-27 09:35:06 -0500

    Update Assignment_6_1.java

--- src/asssignment_6_1/Assignment_6_1.java
+++ src/asssignment_6_1/Assignment_6_1.java
@@ -2,12 +2,20 @@ package asssignment_6_1;
 import java.util.Scanner;
 public class Assignment_6_1 {
     public static String[][] Names(){
-        Scanner input = new Scanner (System.in);
-        System.out.println("What are the names of the passengers ?");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the first and last names of the 36 passengers on the flight: ");
         
-        String[][] flightManifest = new String [4] [9];
-        System.out.println(flightManifest);
-                return flightManifest;
+        String[] [] flightManifest = new String [2] [36];
+        for(int i=0; i<36; i++)
+        {
+            System.out.println("Enter Your Name: ");
+            String name1 = input.next();
+            String name2 = input.next();
+            flightManifest[name1][name2] = input.next();
+        }
+        
+            return flightManifestInput;
     }
     
 }
+
