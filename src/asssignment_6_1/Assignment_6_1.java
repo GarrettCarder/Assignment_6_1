87c07fabfd0ada90316273f1db6d6b82c348fdf6
GarrettCarder <GarrettCarder@users.noreply.github.com>
2015-02-06 09:34:01 -0500

    Update 2/6/15 9:33 AM
    added break & changed string[][] values

--- src/asssignment_6_1/Assignment_6_1.java
+++ src/asssignment_6_1/Assignment_6_1.java
@@ -1,23 +1,33 @@
-package assignment_6_1;
-import java.util.Scanner;
-import java.util.Arrays;
-public class Assignment_6_1 {
+    package assignment_6_1;
+    import java.util.Scanner;
+    import java.util.Arrays;
+    public class Assignment_6_1 {
 
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        String[][]firstAndLastNames = new String[35][1];
-        int[]heightOfPerson = new int[35];
-        for(int i=0; i>35; i++)
-        {
-            System.out.println("Enter Your First Name " + "Passenger #" + i+1 + ": ");
-            firstAndLastNames[i][0] = input.next();
-            System.out.println("Enter Your Last Name" + "Passenger #" + i+1 + ": ");
-            firstAndLastNames[i][1] = input.next();
-            System.out.println("Enter your height (Iches) " + "Passenger #" + i+1 + ": ");
-            heightOfPerson[i] = input.nextInt();
+        public static void main(String[] args) {
+            //Create a Scanner
+            Scanner input = new Scanner(System.in);
+            //Create int & string []
+            String[][]firstAndLastNames = new String[36][2];
+                int[]heightOfPerson = new int[36];
             
+            //Gather list of names and heights
+            for(int i=0; i<heightOfPerson.length; i++)
+            {
+                //Get first name
+                System.out.println("Enter Your First Name " + "Passenger #" + i+1 + ": ");
+                firstAndLastNames[1][i] = input.next();
+                
+                //Get last name
+                System.out.println("Enter Your Last Name " + "Passenger #" + i+1 + ": ");
+                firstAndLastNames[2][i] = input.next();
+                
+                //Get height in inches
+                System.out.println("Enter your height (Iches) " + "Passenger #" + i+1 + ": ");
+                heightOfPerson[i] = input.nextInt();
+                
+                if(firstAndLastNames[36][2] != null){
+                System.out.println(Arrays.deepToString(firstAndLastNames));
+                break;}
+            }
         }
-        System.out.println(firstAndLastNames);
     }
-    
-}
